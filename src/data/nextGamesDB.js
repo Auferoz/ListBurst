4195651383fe29e1ddf4908d1a7033c3d124ef52
Alfredo Saavedra <adesigns7@gmail.com>
2026-02-09 21:35:40 -0300

    feat: add trophy icon and upcoming games data for enhanced dashboard experience

---
+++ src/data/nextGamesDB.js
@@ -0,0 +1,172 @@
+const proximamenteGames = [
+	{
+		title: "MIO: Memories in Orbit",
+		poster: "https://i.3djuegos.com/juegos/19814/mio_memories_in_orbit/fotos/ficha/mio_memories_in_orbit-5901335.webp",
+		dateRelease: "20-01-2026",
+	},
+	{
+		title: "Dispatch",
+		poster: "https://i.3djuegos.com/juegos/20141/dispatch/fotos/ficha/dispatch-5936351.webp",
+		dateRelease: "22-10-2025",
+	},
+	{
+		title: "CODE VEIN II",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co9xhi.webp",
+		dateRelease: "29-01-2026",
+	},
+	{
+		title: "Disciples: Domination",
+		poster: "https://i.3djuegos.com/juegos/20791/disciples_domination/fotos/ficha/disciples_domination-6004935.webp",
+		dateRelease: "12-02-2026",
+	},
+	{
+		title: "ReAnimal",
+		poster: "https://i.3djuegos.com/juegos/19933/reanimal/fotos/ficha/reanimal-5913054.webp",
+		dateRelease: "13-02-2026",
+	},
+	{
+		title: "Resident Evil 9: Requiem",
+		poster: "https://i.3djuegos.com/juegos/20426/resident_evil_9_requiem/fotos/ficha/resident_evil_9_requiem-5965849.webp",
+		dateRelease: "27-02-2026",
+	},
+	{
+		title: "Windstorm: The Legend of Khiimori",
+		poster: "https://i.3djuegos.com/juegos/20004/windstorm_the_legend_of_khiimori/fotos/ficha/windstorm_the_legend_of_khiimori-5924875.webp",
+		dateRelease: "03-03-2026",
+	},
+	{
+		title: "Replaced",
+		poster: "https://i.3djuegos.com/juegos/18035/replaced/fotos/ficha/replaced-5911367.webp",
+		dateRelease: "12-03-2026",
+	},
+	{
+		title: "Crimson Desert",
+		poster: "https://i.3djuegos.com/juegos/16975/crimson_desert/fotos/ficha/crimson_desert-5936467.webp",
+		dateRelease: "19-03-2026",
+	},
+	{
+		title: "Mouse: P.I. For Hire",
+		poster: "https://i.3djuegos.com/juegos/19076/mouse/fotos/ficha/mouse-5994391.webp",
+		dateRelease: "19-03-2026",
+	},
+	{
+		title: "Pragmata",
+		poster: "https://i.3djuegos.com/juegos/17372/pragmata/fotos/ficha/pragmata-5965600.webp",
+		dateRelease: "24-04-2026",
+	},
+	{
+		title: "LEGO Batman: Legacy of The Dark Knight",
+		poster: "https://i.3djuegos.com/juegos/20550/lego_batman_legacy_of_the_dark_knight/fotos/ficha/lego_batman_legacy_of_the_dark_knight-5979251.webp",
+		dateRelease: "29-05-2026",
+	},
+	{
+		title: "Phantom Blade Zero",
+		poster: "https://i.3djuegos.com/juegos/19100/phantom_blade_0/fotos/ficha/phantom_blade_0-6003845.webp",
+		dateRelease: "09-09-2026",
+	},
+	{
+		title: "Forza Horizon 6",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coakbt.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Aeterna Lucis",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co9i7h.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Gears of War: E-Day",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co8bor.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Marvel's Wolverine",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coajxy.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Grand Theft Auto VI",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co9rwo.webp",
+		dateRelease: "18-11-2026",
+	},
+	{
+		title: "Silent Hill: Townfall",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co5l17.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Fable",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co8bop.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "007 First Light",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coaoiz.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Yoshi and the Mysterious Book",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coag5l.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "OD: Knock",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coajod.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Zoopunk",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coav98.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Tides of Annihilation",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coaveg.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "State of Decay 3",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2eby.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "The Blood of Dawnwalker",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co99rj.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "The Mound: Omen of Cthulhu ",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coave2.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Resonance: A Plague Tale Legacy",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co9xuo.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Darksiders 4",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coa7p4.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Marvel 1943: Rise of Hydra",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/coa1rh.webp",
+		dateRelease: "31-12-2026",
+	},
+	{
+		title: "Gear.Club Unlimited 3",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/cob09t.webp",
+		dateRelease: "18-02-2026",
+	},
+	{
+		title: "Tokyo Xtreme Racer",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/co9bk8.webp",
+		dateRelease: "24-09-2025",
+	},
+	{
+		title: "Star Wars: Galactic Racer",
+		poster: "https://images.igdb.com/igdb/image/upload/t_cover_big/cob0lg.webp",
+		dateRelease: "31-12-2026",
+	},
+];
